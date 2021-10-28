350ef7415c26668f1f4782e96abe4f2c4cfd4f7d
Kei Takahashi <ktakahashi.dev@gmail.com>
2021-10-28 07:54:51 -0400

    linting

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es2021": true
+  },
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended"
+  ],
+  "parserOptions": {
+    "ecmaFeatures": {
+      "jsx": true
+    },
+    "ecmaVersion": 12,
+    "sourceType": "module"
+  },
+  "plugins": [
+    "react"
+  ],
+  "settings": {
+    "react": {
+      "version": "detect"
+    }
+  },
+  "rules": {
+    "semi": ["error", "always"],
+    "indent": ["warn", 2],
+    "react/prop-types": "off"
+  }
+};
